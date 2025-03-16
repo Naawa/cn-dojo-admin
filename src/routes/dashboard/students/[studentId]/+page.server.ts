88dc5941bef18bc4130e7afa328159718255346c
Shtanley <hossainsameer117@gmail.com>
2025-03-15 23:18:59 -0700

    fixed remove points

--- src/routes/dashboard/students/[studentId]/+page.server.ts
+++ src/routes/dashboard/students/[studentId]/+page.server.ts
@@ -1,6 +1,6 @@
 import { db } from "$lib/server/db/index.js";
 import { studentSession as studentSessionTable } from "$lib/server/db/schema/session.js";
-import { studentInventory as studentInventoryTable, student as studentTable, studentProfile as studentProfileTable } from "$lib/server/db/schema/student.js";
+import { student as studentTable, studentProfile as studentProfileTable } from "$lib/server/db/schema/student.js";
 import { fail, redirect, type Actions } from "@sveltejs/kit";
 import { eq } from "drizzle-orm";
 
@@ -37,20 +37,20 @@ export const actions: Actions = {
 
         
     },
-    removePoints: async (event) => {
-        const formData = await event.request.formData()
+    removePoints: async ({params, request}) => {
+        const formData = await request.formData()
 
         const currentPoints = parseInt(formData.get('points') as string)
-        const pointsToSubtract = parseInt(formData.get('pointsToSubtract') as string)
-        const studentId = formData.get('studentId') as string
-        if(Number.isNaN(pointsToSubtract)) {
+        const pointsToRemove = parseInt(formData.get('pointsToRemove') as string)
+        const studentId = params.studentId
+        if(Number.isNaN(pointsToRemove)) {
             return fail(400,  { error: "Please enter a number."})
         }
         try {
-            let pointsAdded = await db.update(studentProfileTable).set({points: currentPoints - pointsToSubtract}).where(eq(studentProfileTable.studentId, studentId)).returning({points: studentProfileTable.points})
+            let pointsAdded = await db.update(studentProfileTable).set({points: currentPoints - pointsToRemove}).where(eq(studentProfileTable.studentId, studentId)).returning({points: studentProfileTable.points})
 
-            if(pointsAdded[0].points == pointsToSubtract) {
-                console.log(pointsToSubtract)
+            if(pointsAdded[0].points == pointsToRemove) {
+                console.log(pointsToRemove)
                 return { success: "Sucessfully deducted points!" }
             }
             
@@ -62,8 +62,6 @@ export const actions: Actions = {
 
         await db.delete(studentSessionTable).where(eq(studentSessionTable.studentId, params.studentId as string))
 
-        await db.delete(studentInventoryTable).where(eq(studentInventoryTable.studentId, params.studentId as string))
-
         await db.delete(studentProfileTable).where(eq(studentProfileTable.studentId, params.studentId as string))
 
         await db.delete(studentTable).where(eq(studentTable.id, params.studentId as string))
@@ -71,5 +69,4 @@ export const actions: Actions = {
         throw redirect(302, "/dashboard/students")
 
     },
-    
 };
